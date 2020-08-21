5021d413d7887ec1d709abbc16f27da1406960ba
Sernel123 <sernel.asunto@student.passerellesnumeriques.org>
2020-08-21 15:34:53 +0800

    updated resume

--- src/components/Resume.vue
+++ src/components/Resume.vue
@@ -1,7 +1,8 @@
 <template>
+<div>
   <v-card class="mx-auto mt-8" max-width="800">
      <a
-      href="https://drive.google.com/uc?export=download&id=1mfKLXfXYA64DWdTs6hltdH72PDSHJ1aN"
+      href="https://drive.google.com/uc?export=download&id=1AqCis2Nq8MJ6PBSNJTFbZHJagIwbOwtv"
    
       type="button "
       class="btn btn-primary pdf"
@@ -9,16 +10,22 @@
       
     >Download PDF</a>
    
-    <pdf src="./pdf/resume.pdf"></pdf>
+    <pdf src="./pdf/resumef.pdf"></pdf>
+  
+    <!-- <pdf src="./pdf/resumetwo.pdf"></pdf> -->
   </v-card>
-
+<v-card class="mx-auto mt-8" max-width="800">
+    <pdf src="./pdf/resumetwo.pdf"></pdf>
+  </v-card>
+</div>
 </template>
  
 <script>
 import pdf from "vue-pdf";
 
 export default {
-  name: "resume",
+  name: "resumef",
+  namesec:"resumetwo",
   components: {
     pdf
   }
